9e34ee1f2a6ef3cfcd6113d72be29c9542113dd2
Vinnie Magro <vmagro@socaldevs.com>
2016-03-21 13:22:58 -0700

    First commit on iOS

---
+++ ios/Sprinkler Controller/Sprinkler Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sprinkler Controller
+//
+//  Created by Vinnie Magro on 3/21/16.
+//  Copyright © 2016 Team 12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
