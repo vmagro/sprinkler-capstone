33bd249838f427f0b297f23926ed58f12f3c49f4
Vinnie Magro <vmagro@socaldevs.com>
2016-04-10 14:47:17 -0700

    Link switches to manual controls

--- ios/Sprinkler Controller/Sprinkler Controller/ViewController.swift
+++ ios/Sprinkler Controller/Sprinkler Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var root: Firebase?
     @IBOutlet weak var lastWatering: UILabel!
     @IBOutlet weak var nextWatering: UILabel!
+    @IBOutlet var manualToggles: [UISwitch]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,16 @@ class ViewController: UIViewController {
         root?.childByAppendingPath("schedule").queryOrderedByPriority().queryLimitedToFirst(1).observeSingleEventOfType(.ChildAdded, withBlock: { (snapshot) in
             self.nextWatering.text = snapshot.value as! String
             }, withCancelBlock: nil)
+        
+        root?.childByAppendingPath("manual").observeEventType(.Value, withBlock: { (snapshot) in
+            for (i, toggle) in self.manualToggles.enumerate() {
+                if ((snapshot.value[i] as! String) == "on") {
+                    toggle.on = true
+                } else {
+                    toggle.on = false
+                }
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,6 +50,12 @@ class ViewController: UIViewController {
         dayTimePeriodFormatter.dateFormat = "EEEE, h a"
         
         return dayTimePeriodFormatter.stringFromDate(date)
+    }
+    
+    @IBAction func switchChanged(sender: AnyObject) {
+        let toggle = sender as! UISwitch
+        let index = manualToggles.indexOf(toggle)!
+        root?.childByAppendingPath("manual").childByAppendingPath(String(index)).setValue(toggle.on ? "on" : "off")
     }
 }
 
