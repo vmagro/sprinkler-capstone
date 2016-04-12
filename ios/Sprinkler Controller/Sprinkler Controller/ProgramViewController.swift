170c8bcdc40a4c4a4db9cba6b2e356089ecb1aba
Vinnie Magro <vmagro@socaldevs.com>
2016-04-12 13:39:20 -0700

    Store programs in Firebase

--- ios/Sprinkler Controller/Sprinkler Controller/ProgramViewController.swift
+++ ios/Sprinkler Controller/Sprinkler Controller/ProgramViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ProgramViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ProgramViewController: UIViewController {
 
     var root: Firebase?
     
-    @IBOutlet weak var zonePicker: UIPickerView!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var duration: UITextField!
+    @IBOutlet weak var programName: UITextField!
+    @IBOutlet var labels: [UILabel]!
+    @IBOutlet var sliders: [UISlider]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,35 +27,19 @@ class ProgramViewController: UIViewController, UIPickerViewDataSource, UIPickerV
         // Dispose of any resources that can be recreated.
     }
     
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+    @IBAction func sliderChanged(sender: UISlider) {
+        let index = sliders.indexOf(sender)!
+        labels[index].text = String(Int(sender.value)) + " min"
     }
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if (row == 0) {
-            return "All Zones"
-        }
-        return "Zone " + String(row)
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func didClickAdd(sender: AnyObject) {
-        let zoneChoice = zonePicker.selectedRowInComponent(0)
-        let day = datePicker.calendar.component(.Weekday, fromDate: datePicker.date) - 1
-        let time = datePicker.calendar.component(.Hour, fromDate: datePicker.date) * 60 + datePicker.calendar.component(.Minute, fromDate: datePicker.date)
-        let duration = Double(self.duration.text!)! * 60 * 1000;
-        print("Clicked add", zoneChoice, day, time, duration)
+//        let startTime = datePicker.calendar.component(.Hour, fromDate: datePicker.date) * 60 + datePicker.calendar.component(.Minute, fromDate: datePicker.date)
+        let programName = self.programName.text!
+        let zoneTimes = sliders.map({(s: UISlider) -> Int in return Int(s.value)})
+        root?.childByAppendingPath("programs").childByAppendingPath(programName).setValue([
+            "zones": zoneTimes,
+            "name": programName
+        ])
+        print("Adding program", programName)
     }
 }
