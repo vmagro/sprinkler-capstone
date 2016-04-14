1cf11610fa62a8637e448f4badc09fc18ac72d9c
yoonseon <yoonseon@usc.edu>
2016-04-14 01:38:56 -0700

    connect attiny set interrupt to check moisure
    every 5 sec

--- avr/main.c
+++ avr/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "serial.h"
 
@@ -6,6 +7,8 @@
 #define ZONE_DDR DDRC
 #define ZONE_PORT PORTC
 #define ZONE_START PC1
+#define MOIST_DDR DDRD
+#define MOIST_PORT PORTD
 
 void decode_zone_msg(char* msg, char* zone, char* on) {
   //check to make sure start byte is present
@@ -22,14 +25,23 @@ void decode_zone_msg(char* msg, char* zone, char* on) {
 int main(void) {
   uart_init();
   ZONE_DDR |= (0b1111 << ZONE_START);
-
+  MOIST_DDR &= ~(1<<PD5); //PORTD pin 5 as input
+  MOIST_PORT |= (1<<PD5); //activate pull-ups in PORTD pin5?
+  
   // cycle through turning on all zones on boot
   for (int zone=0; zone < 4; zone++) {
       ZONE_PORT |= (1 << (ZONE_START + zone));
       _delay_ms(200);
       ZONE_PORT &= ~(1 << (ZONE_START + zone));
   }
-
+ 
+ //interrupt timer
+ OCR1A = 0x8CA0; //count up to 36000
+ TCCR1B |= (1<<WGM12); // mode 4, CTC on OCR1A
+ TIMSK1 |= (1<<OCIE1A); //set interrupt on compare match
+ TCCR1B |= (1<<CS12)|(1<<CS10); //set prescaler to 1024 and start the timer
+ sei(); //enable interrupts
+ 
   char buf[3];
 
   while(1) {
@@ -51,3 +63,11 @@ int main(void) {
   return 0;
 }
 
+ISR (TIMER1_COMPA_vect){
+  if(PIND & (1<<PD5)){
+    ZONE_PORT &= ~(1<<PC1);
+    ZONE_PORT &= ~(1<<PC2);
+    ZONE_PORT &= ~(1<<PC3);
+    ZONE_PORT &= ~(1<<PC4);
+  }
+}
