bd63c8fb8a42c0a15e3ecb5f50e59751abf1aa41
Vinnie Magro <vmagro@socaldevs.com>
2016-04-13 22:14:24 -0700

    UART functions untested

--- avr/main.c
+++ avr/main.c
@@ -1,8 +1,12 @@
 #include <avr/io.h>
+#include <util/delay.h>
+#include "serial.h"
 
 int main(void) {
 
   while(1) {
+    uart_send("Hello world");
+    _delay_ms(500);
   }
 
   return 0;
---
+++ avr/serial.c
@@ -0,0 +1,29 @@
+#include <avr/io.h>
+
+#define FOSC 16000000 // Clock frequency
+#define BAUD 9600 // Baud rate used
+#define MYUBRR (FOSC/16/BAUD-1) // Value for UBRR0
+
+void uart_init() {
+  UBRR0 = MYUBRR;
+  UCSR0B |= (1 << TXEN0 | 1 << RXEN0); // Enable RX and TX
+  UCSR0C = (3 << UCSZ00); // Async., no parity,
+  // 1 stop bit, 8 data bits
+}
+
+void uart_send(char* str) {
+  while(*str) {
+    while ((UCSR0A & (1 << UDRE0)) == 0) { }
+    UDR0 = *str;
+    str++;
+  }
+}
+
+void uart_recv(char* buf, int len) {
+  char i = 0;
+  while (i < len) {
+    while(!(UCSR0A & (1<<RXC0))){};
+    buf[i] = len;
+    i++;
+  }
+}
